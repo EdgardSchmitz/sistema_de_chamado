f7564850a1c0dc5f0285905576b1ee6df7bae3db
EdgardSchmitz <edgardschmitz83@gmail.com>
2023-01-17 21:44:47 -0300

    add auterações

---
+++ incorporando_scripts/minha_rede.php
@@ -0,0 +1,5 @@
+<?php
+    include "menu.php";
+?>
+
+Conteudo da página (minha rede)
---
+++ valida_login.php
@@ -0,0 +1,55 @@
+<?php
+
+session_start();
+
+
+
+$usuario_autenticado = false;
+//usuarios do sistema
+$usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+);
+
+/*
+echo '<pre>';
+print_r($usuarios_app);
+'</pre>';*/
+
+foreach($usuarios_app as $user){
+
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+        $usuario_autenticado = true;
+    }
+
+    
+}
+
+if($usuario_autenticado){
+    echo 'Usuário autenticado';
+    $_SESSION['autenticado'] = 'SIM';
+} else{
+    $_SESSION['autenticado'] = 'NAO';
+    header('Location: index.php?login=erro');
+}
+
+
+/*
+print_r($_GET);
+
+echo '<br/>';
+
+echo $_GET['email'];
+echo '<br/>';
+echo $_GET['senha'];*/
+
+/*
+print_r($_POST);
+
+echo '<br/>';
+
+echo $_POST['email'];
+echo '<br/>';
+echo $_POST['senha'];*/
+
+?>
